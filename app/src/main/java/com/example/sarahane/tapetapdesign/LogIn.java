d901ff432a1b1b45114bd891e13168e97fbd9c0b
sarahane <sarahane93@gmail.com>
2017-04-05 15:44:14 +0200

    Design!

--- app/src/main/java/com/example/sarahane/tapetapdesign/LogIn.java
+++ app/src/main/java/com/example/sarahane/tapetapdesign/LogIn.java
@@ -1,7 +1,9 @@
 package com.example.sarahane.tapetapdesign;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LogIn extends AppCompatActivity {
 
@@ -10,4 +12,16 @@ public class LogIn extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
     }
+    //Lägger till så man med hjälp av knapptryck kan komma vidare till nästa
+    //sida. Knapp finns i LogIn (button: log_in: android:onClick="gotoAddUser"/>
+    // Lägg även till i Manifest
+
+    public void gotoAddUser(View v){
+        startActivity(new Intent(LogIn.this, AddUSer.class));
+    }
+
+    public void gotoForgotPass(View v){
+        startActivity(new Intent(LogIn.this, ForgotPass.class));
+    }
 }
+
